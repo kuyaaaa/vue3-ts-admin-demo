632624e4879f86d2d31cd91fa4fe38a966c8127c
朱世俊 <zhushj@qdum.com>
2022-04-22 15:38:14 +0800

    👏feature:登录页面组件实现

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -2,12 +2,75 @@
     <div class="container">
         <n-card class="card-container">
             <n-h1 class="title">this is login</n-h1>
+            <n-form
+                ref="formRef"
+                :label-width="55"
+                label-placement="left"
+                :model="formValue"
+                :rules="rules"
+            >
+                <n-form-item path="userName">
+                    <n-input v-model:value="formValue.userName" placeholder="用户名">
+                        <template #prefix>
+                            <n-icon :component="Person" />
+                        </template>
+                    </n-input>
+                </n-form-item>
+                <n-form-item type="password" path="password">
+                    <n-input
+                        v-model:value="formValue.password"
+                        type="password"
+                        show-password-on="click"
+                        placeholder="密码"
+                    >
+                        <template #prefix>
+                            <n-icon :component="LockClosed" />
+                        </template>
+                    </n-input>
+                </n-form-item>
+                <n-form-item class="btn-row">
+                    <n-button type="primary" size="large" @click="handleLogin">
+                        <template #icon>
+                            <n-icon>
+                                <login-icon />
+                            </n-icon>
+                        </template>
+                        登录
+                    </n-button>
+                </n-form-item>
+            </n-form>
         </n-card>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { NCard, NH1 } from "naive-ui";
+import { NCard, NH1, NForm, NFormItem, NInput, NIcon, NButton } from "naive-ui";
+import type { FormInst } from "naive-ui";
+import { Person, LockClosed, LogIn as loginIcon } from "@vicons/ionicons5";
+import { ref } from "vue";
+
+const formRef = ref<FormInst | null>(null);
+const formValue = ref({
+    userName: "",
+    password: "",
+});
+const rules = ref({
+    userName: {
+        required: true,
+        message: "请输入用户名",
+        trigger: "blur",
+    },
+    password: { required: true, message: "请输入密码", trigger: "blur" },
+});
+const handleLogin = (e: MouseEvent) => {
+    e.preventDefault();
+    formRef.value?.validate(errors => {
+        if (!errors) {
+            // todo login
+            console.log("ok");
+        }
+    });
+};
 </script>
 
 <style lang="scss" scoped>
@@ -21,10 +84,15 @@ import { NCard, NH1 } from "naive-ui";
 
 .card-container {
     width: 540px;
+    padding: 0 20px;
     @include box-shadow-base;
 
     .title {
         text-align: center;
+    }
+
+    .btn-row {
+        @include flex-center-main-cross;
     }
 }
 </style>
