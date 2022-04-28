3d688dbd3e32a36886200dcbc73d9ee2673dd5f3
朱世俊 <zhushj@qdum.com>
2022-04-28 14:18:15 +0800

    👏feature:路由拦截token检测

--- src/router/guards.ts
+++ src/router/guards.ts
@@ -1,7 +1,22 @@
 // 路由守卫
 import router from "./index";
+import { TOKEN } from "@/utils/static";
 
-router.beforeEach((to, from) => {
-    console.log("to", to);
-    console.log("from", from);
+const token = window.localStorage.getItem(TOKEN);
+/** 白名单 直接跳过的路由路径 */
+const whiteList = ["/login"];
+
+router.beforeEach((to, from, next) => {
+    // 白名单检测
+    const isWhite = whiteList.some(item => item === to.fullPath);
+    if (isWhite) {
+        next();
+        return;
+    }
+    // token检测
+    if (token) {
+        next();
+    } else {
+        window.$router.push("/login");
+    }
 });
