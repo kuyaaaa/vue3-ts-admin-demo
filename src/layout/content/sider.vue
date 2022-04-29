d28437cbe8d27d795cf96fdc89e864e22ece2f85
朱世俊 <zhushj@qdum.com>
2022-04-29 15:57:35 +0800

    👏feature:侧栏添加logo位置

--- src/layout/content/sider.vue
+++ src/layout/content/sider.vue
@@ -1,9 +1,9 @@
 <template>
     <n-layout-sider
         collapse-mode="width"
-        :collapsed-width="64"
+        :collapsed-width="collapsedWidth"
         :collapsed="collapsed"
-        :width="240"
+        :width="siderWidth"
         :native-scrollbar="false"
         class="layout-sider"
         bordered
@@ -11,24 +11,37 @@
         @collapse="collapsed = true"
         @expand="collapsed = false"
     >
+        <n-space :wrap="false" class="logo-container" align="center" justify="center">
+            <n-image
+                :width="collapsedIconSize"
+                src="/src/assets/images/logo.png"
+                preview-disabled
+            />
+            <n-text v-if="!collapsed" class="text-nowrap">COOL LOGO!</n-text>
+        </n-space>
         <n-menu
             :options="menuOptions"
             :value="currentMenu"
             :collapsed="collapsed"
-            :collapsed-width="64"
-            :collapsed-icon-size="22"
+            :collapsed-width="collapsedWidth"
+            :collapsed-icon-size="collapsedIconSize"
         />
     </n-layout-sider>
 </template>
 
 <script lang="ts" setup>
 import { computed, ref } from "vue";
-import { NLayoutSider, NMenu } from "naive-ui";
+import { NLayoutSider, NMenu, NSpace, NImage, NText } from "naive-ui";
 import useMenuStore from "@/store/modules/menu";
 
 const menuStore = useMenuStore();
 
 const menuOptions = ref<any[]>([]);
+
+// 部分配置
+const siderWidth = 240;
+const collapsedWidth = 64;
+const collapsedIconSize = 22;
 
 if (!menuStore.list.length) {
     menuStore.createMenuList();
@@ -43,4 +56,11 @@ const currentMenu = computed(() => {
 const collapsed = ref(false);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo-container {
+    height: v-bind('(collapsedIconSize + 10) + "px"');
+    padding: 10px 0;
+    font-size: 22px;
+    font-weight: 700;
+}
+</style>
