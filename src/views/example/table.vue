723eca7fc0b0898b781362f8a6e9ff62fb0502c5
朱世俊 <zhushj@qdum.com>
2022-05-18 17:14:30 +0800

    👏feature:表格页面完善

--- src/views/example/table.vue
+++ src/views/example/table.vue
@@ -1,11 +1,58 @@
 <template>
-    <n-data-table :columns="columns" :data="data" :single-line="false" :pagination="pagination" />
+    <n-card class="content-card">
+        <n-grid x-gap="12" :cols="5">
+            <n-gi>
+                <n-input
+                    v-model:value="searchParams.keyword"
+                    type="text"
+                    clearable
+                    placeholder="请输入关键字"
+                />
+            </n-gi>
+            <n-gi :span="2">
+                <n-input-group>
+                    <n-date-picker v-model:value="searchParams.date" clearable />
+                    <n-time-picker v-model:value="searchParams.time" clearable />
+                </n-input-group>
+            </n-gi>
+            <n-gi :offset="1" style="text-align: right">
+                <n-button type="primary">
+                    <template #icon>
+                        <n-icon>
+                            <search-icon />
+                        </n-icon>
+                    </template>
+                    搜索
+                </n-button>
+            </n-gi>
+        </n-grid>
+    </n-card>
+    <n-card class="content-card">
+        <n-data-table
+            :columns="columns"
+            :data="data"
+            :single-line="false"
+            :pagination="pagination"
+        />
+    </n-card>
 </template>
 
 <script lang="ts" setup>
-import { h, ref } from "vue";
-import { NDataTable, NButton } from "naive-ui";
+import { h, reactive, ref } from "vue";
+import {
+    NCard,
+    NGrid,
+    NGi,
+    NDataTable,
+    NIcon,
+    NButton,
+    NInput,
+    NInputGroup,
+    NDatePicker,
+    NTimePicker,
+} from "naive-ui";
 import type { PaginationProps, DataTableColumns } from "naive-ui";
+import { Search as SearchIcon } from "@vicons/ionicons5";
 
 type Song = {
     no: number;
@@ -74,6 +121,12 @@ const columns: DataTableColumns<Song> = [
         },
     },
 ];
+
+const searchParams = reactive({
+    keyword: "",
+    date: null,
+    time: null,
+});
 </script>
 
 <style lang="scss" scoped></style>
