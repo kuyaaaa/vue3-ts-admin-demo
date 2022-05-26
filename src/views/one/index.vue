07b0691be6a103996ec7177c3cd228e0a5d8b343
朱世俊 <qq1034816916@163.com>
2022-05-26 17:56:22 +0800

    🔧refactor:重写单级菜单描述

--- src/views/one/index.vue
+++ src/views/one/index.vue
@@ -2,16 +2,21 @@
     <n-card class="card-container">
         <n-p>单级菜单需要在router中如下配置：</n-p>
         <n-code :code="oneRouteCode" language="javascript" />
-        <n-p>单级菜单的所有属性在<code> children </code>的<code> meta </code>中定义。</n-p>
+        <n-p
+            >单级菜单的所有属性在<n-text code>children</n-text>的<n-text code>meta</n-text
+            >中定义。</n-p
+        >
         <n-p>
-            必须给单级菜单包裹在一层<code> Layout </code>组件下的<code> children </code
-            >中，并且在外层路由的<code> meta </code>中定义<code> onlyFirst </code>字段标识。
+            必须给单级菜单包裹在一层<n-text code>Layout</n-text>组件下的<n-text code
+                >children</n-text
+            >中，并且在外层路由的<n-text code>meta</n-text>中定义<n-text code>onlyFirst</n-text
+            >字段标识。
         </n-p>
     </n-card>
 </template>
 
 <script lang="ts" setup>
-import { NCard, NCode, NP } from "naive-ui";
+import { NCard, NCode, NP, NText } from "naive-ui";
 
 const oneRouteCode = `{
     path: "/one",
