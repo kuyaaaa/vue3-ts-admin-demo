3aecf3cfa4c6cc631cb9523bc80da78c7d0d09ff
朱世俊 <qq1034816916@163.com>
2022-05-26 17:59:47 +0800

    🔧refactor:描述页面文字添加一个好康的条

--- src/views/many/docs.vue
+++ src/views/many/docs.vue
@@ -2,21 +2,24 @@
     <n-card class="card-container">
         <n-p>多级需要在router中如下配置：</n-p>
         <n-code :code="oneRouteCode" language="javascript" />
-        <n-p>
-            最外层路由的<n-text code>component</n-text>需要为<n-text code>Layout</n-text
-            >，而作为中间层的过渡元素的<n-text code>component </n-text>需要为<n-text code
-                >MultistageRouterLayout</n-text
-            >即
-            <n-text code>@/components/multistage-router-layout.vue</n-text
-            >组件，该组件只有一个<n-text code>router-view</n-text> 标签，如果不添加该<n-text code
-                >component</n-text
-            >，多级嵌套下的页面会连同布局容器一直套娃。
-        </n-p>
+        <n-blockquote>
+            <n-p>
+                最外层路由的<n-text code>component</n-text>需要为<n-text code>Layout</n-text
+                >，而作为中间层的过渡元素的<n-text code>component </n-text>需要为<n-text code
+                    >MultistageRouterLayout</n-text
+                >即
+                <n-text code>@/components/multistage-router-layout.vue</n-text
+                >组件，该组件只有一个<n-text code>router-view</n-text> 标签，如果不添加该<n-text
+                    code
+                    >component</n-text
+                >，多级嵌套下的页面会连同布局容器一直套娃。
+            </n-p>
+        </n-blockquote>
     </n-card>
 </template>
 
 <script lang="ts" setup>
-import { NCard, NCode, NP, NText } from "naive-ui";
+import { NCard, NCode, NP, NText, NBlockquote } from "naive-ui";
 
 const oneRouteCode = `
 {
--- src/views/one/index.vue
+++ src/views/one/index.vue
@@ -2,21 +2,23 @@
     <n-card class="card-container">
         <n-p>单级菜单需要在router中如下配置：</n-p>
         <n-code :code="oneRouteCode" language="javascript" />
-        <n-p
-            >单级菜单的所有属性在<n-text code>children</n-text>的<n-text code>meta</n-text
-            >中定义。</n-p
-        >
-        <n-p>
-            必须给单级菜单包裹在一层<n-text code>Layout</n-text>组件下的<n-text code
-                >children</n-text
-            >中，并且在外层路由的<n-text code>meta</n-text>中定义<n-text code>onlyFirst</n-text
-            >字段标识。
-        </n-p>
+        <n-blockquote>
+            <n-p
+                >单级菜单的所有属性在<n-text code>children</n-text>的<n-text code>meta</n-text
+                >中定义。</n-p
+            >
+            <n-p>
+                必须给单级菜单包裹在一层<n-text code>Layout</n-text>组件下的<n-text code
+                    >children</n-text
+                >中，并且在外层路由的<n-text code>meta</n-text>中定义<n-text code>onlyFirst</n-text
+                >字段标识。
+            </n-p>
+        </n-blockquote>
     </n-card>
 </template>
 
 <script lang="ts" setup>
-import { NCard, NCode, NP, NText } from "naive-ui";
+import { NCard, NCode, NP, NText, NBlockquote } from "naive-ui";
 
 const oneRouteCode = `{
     path: "/one",
