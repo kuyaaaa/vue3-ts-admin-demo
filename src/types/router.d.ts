c5ad8307d24840e7cf1bfa4636d1629f4b1f3ad1
朱世俊 <qq1034816916@163.com>
2022-05-27 09:44:09 +0800

    🔧refactor:RouterMeta定义注释修改

--- src/types/router.d.ts
+++ src/types/router.d.ts
@@ -4,22 +4,21 @@ import "vue-router";
 import type { Component } from "vue";
 
 declare module "vue-router" {
-    /**
-     * meta参数定义
-     * @param   label       对应菜单名称
-     * @param   icon        图标，需从"@vicons/ionicons5"引入
-     * @param   hidden      是否在菜单中隐藏
-     * @param   disabled    是否在菜单中禁用
-     * @param   onlyFirst   是否只为一级菜单
-     *                      若为true，只会取当前路由children下第一个元素替换为一级菜单
-     * @param   outLink     如果为外链，填外链地址
-     */
     interface RouteMeta {
+        /** 对应菜单名称 */
         label?: string;
+        /** 图标，需从"@vicons"引入 */
         icon?: Component;
+        /** 是否在菜单中隐藏 */
         hidden?: boolean;
+        /** 是否在菜单中禁用 */
         disabled?: boolean;
+        /**
+         * 是否只为一级菜单
+         * 若为true，只会取当前路由children下第一个元素替换为一级菜单
+         */
         onlyFirst?: boolean;
+        /** 如果为外链，填外链地址 */
         outLink?: string;
     }
 }
