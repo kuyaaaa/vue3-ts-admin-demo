a1b1c58c925c80261a641c2cfab18eb106e38d8f
朱世俊 <qq1034816916@163.com>
2022-06-07 15:07:46 +0800

    ⚡fix:错误页面适用暗黑模式

--- src/views/errors/403.vue
+++ src/views/errors/403.vue
@@ -1,9 +1,11 @@
 <template>
-    <n-result status="403" size="huge" title="403 禁止访问" description="总有些门是对你关闭的">
-        <template #footer>
-            <n-button @click="handleReturn">回到过去</n-button>
-        </template>
-    </n-result>
+    <n-layout style="height: 100%">
+        <n-result status="403" size="huge" title="403 禁止访问" description="总有些门是对你关闭的">
+            <template #footer>
+                <n-button @click="handleReturn">回到过去</n-button>
+            </template>
+        </n-result>
+    </n-layout>
 </template>
 
 <script lang="ts" setup>
--- src/views/errors/404.vue
+++ src/views/errors/404.vue
@@ -1,9 +1,11 @@
 <template>
-    <n-result status="404" size="huge" title="404 资源不存在" description="生活总归带点荒谬">
-        <template #footer>
-            <n-button @click="handleReturn">回到过去</n-button>
-        </template>
-    </n-result>
+    <n-layout style="height: 100%">
+        <n-result status="404" size="huge" title="404 资源不存在" description="生活总归带点荒谬">
+            <template #footer>
+                <n-button @click="handleReturn">回到过去</n-button>
+            </template>
+        </n-result>
+    </n-layout>
 </template>
 
 <script lang="ts" setup>
