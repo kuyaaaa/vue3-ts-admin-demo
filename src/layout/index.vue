982681ac1a44e431330041fa534e57b0f1b024ae
朱世俊 <qq1034816916@163.com>
2023-02-06 10:58:06 +0800

    🔧refactor:resize hook提取

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -29,53 +29,22 @@
 </template>
 
 <script lang="ts" setup>
-import { onBeforeUnmount, onMounted, ref } from "vue";
-import { throttle } from "lodash-es";
 import { storeToRefs } from "pinia";
 import { NLayoutContent, NLayoutHeader } from "naive-ui";
 import HeaderContent from "./content/header.vue";
 import SiderContent from "./content/sider.vue";
-import useSystemStore from "@/store/modules/system";
 import useHistoryStore from "@/store/modules/history";
-
-const style_content_padding = 20;
-const style_content_height = ref("100%");
-
-const systemStore = useSystemStore();
+import useResizeHook from "@/hooks/resize";
 
 const historyStore = useHistoryStore();
 const { keepAliveInclude } = storeToRefs(historyStore);
 
-const headerRef = ref<InstanceType<typeof NLayoutHeader> | null>(null);
-const contentRef = ref<InstanceType<typeof NLayoutContent> | null>(null);
-
-const resizeHandler = throttle(() => {
-    const headerEl: HTMLElement = headerRef.value?.$el;
-    const contentEl: HTMLElement = contentRef.value?.$el;
-
-    const headerHeight = headerEl.offsetHeight;
-    const contentHeight = window.innerHeight - headerHeight;
-    const contentBorderBoxHeight = contentHeight - style_content_padding * 2;
-    const contentWidth = contentEl.offsetWidth - style_content_padding * 2;
-
-    style_content_height.value = `${contentHeight}px`;
-
-    systemStore.$patch({ headerHeight, contentHeight, contentBorderBoxHeight, contentWidth });
-}, 200);
+const { style_content_padding, style_content_height, headerRef, contentRef, changeContentWidth } =
+    useResizeHook({ contentPadding: 20 });
 
 const handleSiderCollapse = (collapsed: boolean) => {
-    const contentEl = contentRef.value?.$el as HTMLElement;
-    systemStore.contentWidth = contentEl.offsetWidth - style_content_padding * 2;
+    changeContentWidth();
 };
-
-onMounted(() => {
-    window.addEventListener("resize", resizeHandler);
-    resizeHandler();
-});
-
-onBeforeUnmount(() => {
-    window.removeEventListener("resize", resizeHandler);
-});
 </script>
 
 <style lang="scss" scoped>
